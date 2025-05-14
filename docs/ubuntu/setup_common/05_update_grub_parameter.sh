2a6853ab3a1effa480e21736b89ea00cc5f00398
tk4terui <tk4terui@gmail.com>
2025-05-14 15:08:14 +0900

    update grub parameter

--- docs/ubuntu/setup_common/05_update_grub_parameter.sh
+++ docs/ubuntu/setup_common/05_update_grub_parameter.sh
@@ -10,4 +10,4 @@ sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=8/' /etc/default/grub
 sed -i "s/GRUB_CMDLINE_LINUX=\".*/GRUB_CMDLINE_LINUX=\"${KERNEL_PARAMETER}\"/" /etc/default/grub
 
 # Generate grub file with updated parameters 
-#update-grub
+update-grub
